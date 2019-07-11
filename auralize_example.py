650b5be72d3c65605d3a1d2f819ca5cce6cd7280
southernsun <southernsun@users.noreply.github.com>
2019-07-11 11:12:56 +0200

    Added example data in root folder.

--- auralize_example.py
+++ auralize_example.py
@@ -3,7 +3,7 @@ from samples.auralize_from_content import auralize_from_content
 
 if __name__ == "__main__":
     # Run Auralization with object structure build within python.
-    # auralize_from_env_example()
+    auralize_from_env_example()
 
     # Run Auralization with using an input file;
-    auralize_from_content()
+    auralize_from_content("environ.txt")
